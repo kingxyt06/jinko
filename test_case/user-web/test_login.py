fdc9eb3d94d7412a7fc0f3f2a19cffcc86673159
徐英涛 <xuyingtao@linklogis.com>
2024-09-19 19:57:07 +0800

    通用型改造...

--- test_case/user-web/test_login.py
+++ test_case/user-web/test_login.py
@@ -1,12 +1,10 @@
-import json
 import re
 import time
 
 import pytest
 import requests
-from redis import Redis
-from config.conf import ConfigReader
-from utils.LoadData import replace_value
+
+from utils.LoadData import replace_value, standard_yaml, send_request
 from utils.RedisUtil import RedisUtill
 from utils.YamlUtil import YamlUtil
 
@@ -29,30 +27,35 @@ class TestLogin:
         if "extract" in args.keys():
             for key, value in args["extract"].items():
                 values = {'picCheckCodeKey': picCheckCodeKey, 'picCheckCode': picCheckCode}
-                if "(.*?)" in value or "(.+?)" in value:
-                    if key in values:
-                        extract_value = {key: values[key]}
-                        YamlUtil().write_yaml(extract_value)
+                if key in values:
+                    extract_value = {key: values[key]}
+                    YamlUtil().write_yaml(extract_value)
+
+    # @pytest.mark.parametrize('args', YamlUtil().read_data('/test_data/user-web/login_agw.yaml'))
+    # def test_login(self, args):
+    #     method = args['request'].pop('method')
+    #     url = args['request'].pop('url')
+    #     data = replace_value(args['request'])
+    #
+    #     res = requests.session().request(method=method, url="https://jinkoscf-agw-qa.llsstd.com/" + url
+    #                                      , json=data['json'], cookies=data['cookies'])
+    #     cookies_sub = []
+    #     cookies_dict = requests.utils.dict_from_cookiejar(res.cookies)
+    #     for key, value in cookies_dict.items():
+    #         cookies_sub.append({key: value})
+    #
+    #     if "extract" in args.keys():
+    #         for key, value in args["extract"].items():
+    #             for k, v in value.items():
+    #                 for ck in cookies_sub:
+    #                     if k in ck:
+    #                         args["extract"][key][k] = ck[k]
+    #     YamlUtil().write_yaml(args["extract"])
 
     @pytest.mark.parametrize('args', YamlUtil().read_data('/test_data/user-web/login_agw.yaml'))
-    def test_login(self, args):
-        method = args['request'].pop('method')
-        url = args['request'].pop('url')
-        data = replace_value(args['request'])
-        # if isinstance(args['request'], dict):
-        #     str_data = json.dumps(args['request'])
-        #     for i in range(1, str_data.count("${") + 1):
-        #         if '${' and '}' in str_data:
-        #             start_index = str_data.index("${")
-        #             end_index = str_data.index("}", start_index)
-        #             old_value = str_data[start_index:end_index + 1]
-        #             new_value = YamlUtil().read_yaml(old_value[2:-1])
-        #             str_data = str_data.replace(old_value, new_value)
-        #     if isinstance(str_data, str):
-        #         data = json.loads(str_data)
+    def test_login(self,args):
+        res = standard_yaml(args)
 
-        res = requests.session().request(method=method, url="https://jinkoscf-agw-qa.llsstd.com/" + url
-                                         , json=data['json'], cookies=data['cookies'])
         cookies_sub = []
         cookies_dict = requests.utils.dict_from_cookiejar(res.cookies)
         for key, value in cookies_dict.items():
--- utils/LoadData.py
+++ utils/LoadData.py
@@ -1,5 +1,6 @@
 import json
 
+import requests
 import yaml
 
 from utils.YamlUtil import YamlUtil
@@ -8,49 +9,71 @@ from utils.YamlUtil import YamlUtil
 def load(path):
     file = open(path, 'r', encoding='utf-8')
     data = yaml.load(file, Loader=yaml.FullLoader)
+    print(data)
+    # standard_yaml(data)
     # 因为读取的yaml数据是列表，需要转化为dict，但这又是个不规则的列表里面嵌着字典，所以需要特殊处理
-    for data_item in data:
-        data_keys = data_item.keys()
-        print(f"数据项的键：{data_keys}")
+    # for data_item in data:
+    #     data_keys = data_item.keys()
+    #     print(f"数据项的键：{data_keys}")
+    #
+    # if 'name' in data_keys and 'request' in data_keys and 'validate' in data_keys:
+    #     request_keys = data_item['request'].keys()
+    #     req_kwargs = data_item['request']
+    #     if 'method' in request_keys and 'url' in request_keys:
+    #         print("yaml基础结构检查正确")
+    #         method = req_kwargs.pop('method')
+    #         url = req_kwargs.pop('url')
+    #         # 处理请求参数
+    #         for key, value in req_kwargs.items():
+    #             if key in ['params', 'data', 'json', 'headers', 'cookies']:
+    #                 req_kwargs[key] = replace_value(value)
 
-    if 'name' in data_keys and 'request' in data_keys and 'validate' in data_keys:
-        request_keys = data_item['request'].keys()
-        req_kwargs = data_item['request']
-        if 'method' in request_keys and 'url' in request_keys:
+
+def send_request(method, url, **kwargs):
+    method = str(method).lower()  # 转换小写
+    # 基础路径的拼接和替换
+    url = "https://jinkoscf-agw-qa.llsstd.com/" + url
+    for key, value in kwargs.items():
+        if key in ['params', 'data', 'json', 'headers', 'cookies']:
+            kwargs[key] = replace_value(value)
+    res = requests.session().request(method, url, **kwargs)
+    return res
+
+
+def standard_yaml(caseinfo):
+    caseinfo_keys = caseinfo.keys()
+    print(caseinfo_keys)
+    if 'name' in caseinfo_keys and 'request' in caseinfo_keys and 'validate' in caseinfo_keys:
+        request_keys = caseinfo['request'].keys()
+        if 'method' and 'url' in request_keys:
             print("yaml基础结构检查正确")
-            method = req_kwargs.pop('method')
-            url = req_kwargs.pop('url')
-            # 处理请求参数
-            for key, value in req_kwargs.items():
-                if key in ['params', 'data', 'json', 'headers', 'cookies']:
-                    req_kwargs[key] = replace_value(value)
+            method = caseinfo['request'].pop('method')
+            url = caseinfo['request'].pop('url')
+            res = send_request(method, url, **caseinfo['request'])
+            return res
 
 
 def replace_value(data):
     # print(type(data))
-    if isinstance(data, dict) or isinstance(data, list):
-        str_data = json.dumps(data)
-    else:
-        str_data = str(data)
+    if data:
+        if isinstance(data, dict) or isinstance(data, list):
+            str_data = json.dumps(data)
+        else:
+            str_data = str(data)
 
-    for cs in range(1, str_data.count('${') + 1):
-        if '${' in str_data and '}' in str_data:
-            start_index = str_data.index('${')
-            end_index = str_data.index('}', start_index)
-            old_value = str_data[start_index:end_index+1]
-            new_value = YamlUtil().read_yaml(old_value[2:-1])
-            str_data = str_data.replace(old_value, new_value)
-    if isinstance(str_data, str):
-        data = json.loads(str_data)
-    return data
+        for cs in range(1, str_data.count('${') + 1):
+            if '${' in str_data and '}' in str_data:
+                start_index = str_data.index('${')
+                end_index = str_data.index('}', start_index)
+                old_value = str_data[start_index:end_index + 1]
+                new_value = YamlUtil().read_yaml(old_value[2:-1])
+                str_data = str_data.replace(old_value, new_value)
+        if isinstance(str_data, str):
+            data = json.loads(str_data)
+        return data
 
 
 if __name__ == '__main__':
-    # cookies = {'cookies':""}
-    # res = requests.Session().request(method,
-    #               url,
-    #               cookies=cookies,
-    #               **data_item['request'],
-    #               )
-    # print(res)
-    load('../test_data/asset-web/saveAsset.yaml')
+    res = YamlUtil().read_data('/test_data/user-web/CheckCode.yaml')
+    print(type(res))
+    # load('../test_data/user-web/login_agw.yaml')
